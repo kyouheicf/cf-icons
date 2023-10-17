4b35f87eb37ee9a45ed91ad24a1122a3fac2d734
kyouheicf <85217388+kyouheicf@users.noreply.github.com>
2023-10-17 12:32:03 +0900

    initial commit

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,10 +1,12 @@
 const svgServe = async ({ request, env }) => {
     const url = new URL(request.url)
-    const extension = url.pathname.split('/').reverse()[0].split('.')[1]
+    const file = url.pathname.split('/').reverse()[0]
+    const filename = file.split('.')[0]
+    const extension = file.split('.')[1]
     console.log(extension)
 
     if (extension === 'svg') {
-        return new Response('Ok');
+        return fetch(`https://imagedelivery.net/${env.ACCOUNT_HASH}/${filename}/${extension}`)
     }
 
     return env.ASSETS.fetch(url)
