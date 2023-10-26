e48d00a0194e0462041746801c39059120ff4743
kyouheicf <85217388+kyouheicf@users.noreply.github.com>
2023-10-26 11:42:51 +0900

    Update

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,10 +1,10 @@
 const svgServe = async ({ request, env }) => {
     const url = new URL(request.url)
     const file = url.pathname.split('/').reverse()[0]
-    let filename = file.split('.').splice(-1)
+    let filename = file.split('.')
     filename.splice(-1)
     const extension = file.split('.').reverse()[0]
-    console.log(`filename === ${filename}`)
+    console.log(`filename === ${filename.join('.')}`)
     console.log(`extension === ${extension}`)
 
     if (extension === 'svg') {
